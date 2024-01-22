ef44fbef1c34fbe70c6e98f11e3333f78a4cfff8
sushma <sushma.shukla@renewbuy.com>
2024-01-22 16:21:54 +0530

    ADD: Initail add core module for the main services and guards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { WindowRef } from './services/window-ref.service';
-import { AuthService } from './services/auth.service';
+
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
@@ -12,7 +11,7 @@ import { environment } from 'src/environments/environment';
 export class AppComponent {
   title = 'apex_frontend';
 
-  constructor(private win: WindowRef, private authService: AuthService,) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
