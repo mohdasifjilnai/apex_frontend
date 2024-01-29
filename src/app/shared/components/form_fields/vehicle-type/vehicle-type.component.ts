bacca2f82470d4467d68735cc39912db6035fa57
shivam <shivam.srivastava@renewbuy.com>
2024-01-29 09:46:19 +0530

    ADD:Vehicle List UI

--- src/app/shared/components/form_fields/vehicle-type/vehicle-type.component.ts
+++ src/app/shared/components/form_fields/vehicle-type/vehicle-type.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehicle-type.component.scss']
 })
 export class VehicleTypeComponent implements OnInit {
+  vehicleTypeList:any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.vehicleTypeList = [
+      {
+        optionName: 'Two Wheeler',
+        imageUrl: '/assets/icon/two-wheeler.svg',
+        imageInformation: 'Two Wheeler',
+      },
+      {
+        optionName: 'Private Car',
+        imageUrl: '/assets/icon/private-car.svg',
+        imageInformation: 'Private Car',
+      },
+      {
+        optionName: 'Commercial Vehicle',
+        imageUrl: '/assets/icon/commercial-vehicle.svg',
+        imageInformation: 'Commercial Vehicle',
+      },
+    ];
   }
 
 }
