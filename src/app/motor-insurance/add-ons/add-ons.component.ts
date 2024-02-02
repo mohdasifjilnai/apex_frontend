f58a4455eadef028177ed65df818348c06552056
Mohd Asif Jilani <mohdasif.jilani@renewbuy.com>
2024-02-02 10:52:55 +0000

    ADD :Create Addons component

--- src/app/motor-insurance/add-ons/add-ons.component.ts
+++ src/app/motor-insurance/add-ons/add-ons.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import add_ons_list from './add-ons-list.json'
 @Component({
   selector: 'app-add-ons',
   templateUrl: './add-ons.component.html',
   styleUrls: ['./add-ons.component.scss'],
 })
 export class AddOnsComponent implements OnInit {
+  add_ons_list:any=add_ons_list
   constructor() {}
 
   ngOnInit(): void {}
+
+  /**
+   * this fucntion use for clear all check box to uncheck
+   */ 
+  clearAllChecked(): void {
+    this.add_ons_list.forEach((item:any) => (item.checked = false));
+  }
 }
