4aaeecb810c7762a0a6ae203a7b181c72cc6daa5
Krishna Kumar <krishna.kumar@renewbuy.com>
2024-02-07 10:13:40 +0000

    ADD :Dropdown and share button in quotation page

--- src/app/motor-insurance/quotes-listing/quotes-listing.component.ts
+++ src/app/motor-insurance/quotes-listing/quotes-listing.component.ts
@@ -1,25 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import initiate_quotes_payload from './initiate_quotes_payload.json';
-import {ApiConstants} from '../../api.constant'
+import { ApiConstants } from '../../api.constant';
 import { ApiService } from 'src/app/core/services/api.service';
-import {PremiumBreakupComponent} from '../../shared/components/dialog-components/premium-breakup/premium-breakup.component'
+import { PremiumBreakupComponent } from '../../shared/components/dialog-components/premium-breakup/premium-breakup.component';
 import { WindowRef } from 'src/app/core/services/window-ref.service';
 import { MatDialog } from '@angular/material/dialog';
+import { MatTabChangeEvent } from '@angular/material/tabs';
 @Component({
   selector: 'app-quotes-listing',
   templateUrl: './quotes-listing.component.html',
   styleUrls: ['./quotes-listing.component.scss'],
 })
 export class QuotesListingComponent implements OnInit {
-  initiateQuotes:any;
-  constructor(private router : Router,private apiService:ApiService,public matDialog: MatDialog ) {
-    this.postListInitiateQuotes(initiate_quotes_payload)
+  initiateQuotes: any;
+  constructor(
+    private router: Router,
+    private apiService: ApiService,
+    public matDialog: MatDialog
+  ) {
+    this.postListInitiateQuotes(initiate_quotes_payload);
   }
 
+  noQuotesInformation: any;
 
-  noQuotesInformation:any;
-  
   ngOnInit(): void {}
 
   getProposalDetails() {
@@ -28,36 +32,44 @@ export class QuotesListingComponent implements OnInit {
   /**
    * this function is used for the no quotes information details
    */
-  noQuotes(){
-    this.noQuotesInformation = !this.noQuotesInformation
+  noQuotes() {
+    this.noQuotesInformation = !this.noQuotesInformation;
   }
-    /**
+  /**
    * get initiate quotes list
    */
 
-     postListInitiateQuotes(data:any) {
-        this.apiService.postRequestedResponse(ApiConstants.initiate_quotes,data).subscribe((res:any)=>{
-          this.initiateQuotes=res;
-          console.log(res,'res')
-        })
-    }
-
-    /**
-     * Open premium breakup modal
-     */ 
-     openPremiumBreakupModal(initiateQuotes:any,event: MouseEvent){
-      console.log(initiateQuotes,'initiateQuotes');
-      
-      this.matDialog.open(PremiumBreakupComponent, {
-        panelClass:'initiate-quotes-class',
-        data: {
-          initiateQuotes
-        },
-        position: {
-          top: `${event.clientY}px`,
-          left: `${event.clientX}px`
-        }
+  postListInitiateQuotes(data: any) {
+    this.apiService
+      .postRequestedResponse(ApiConstants.initiate_quotes, data)
+      .subscribe((res: any) => {
+        this.initiateQuotes = res;
       });
-     }
-  
+  }
+
+  /**
+   * Open premium breakup modal
+   */
+  openPremiumBreakupModal(initiateQuotes: any, event: MouseEvent) {
+    this.matDialog.open(PremiumBreakupComponent, {
+      panelClass: 'initiate-quotes-class',
+      data: {
+        initiateQuotes,
+      },
+      position: {
+        top: `${event.clientY}px`,
+        left: `${event.clientX}px`,
+      },
+    });
+  }
+  showComprehensiveDiv = true;
+  individualSelected = 'Individual';
+  lowHighSelected = 'low';
+  onComprehensiveTabChange(event: MatTabChangeEvent): void {
+    if (event.index === 1) {
+      this.showComprehensiveDiv = false;
+    } else {
+      this.showComprehensiveDiv = true;
+    }
+  }
 }
