a447d47478b5d53401074eaee2b1e1da01cc431d
Krishna Kumar <krishna.kumar@renewbuy.com>
2024-03-29 12:47:00 +0000

    FIX : Stepper in Resposive on Proposal Page

--- src/app/motor-insurance/proposal/proposal.component.ts
+++ src/app/motor-insurance/proposal/proposal.component.ts
@@ -95,6 +95,40 @@ export class ProposalComponent implements OnInit {
     }
   }
 
+  step(stepper:any){
+    if(stepper=='ckyc'){
+      this.step1=true
+      this.step2=false
+      this.step3=false
+      this.step4=false
+      this.step5=false
+    }else if (stepper === 'Vehicle Owner Details') {
+      this.step1=false
+      this.step2=true
+      this.step3=false
+      this.step4=false
+      this.step5=false
+    } else if (stepper === 'Nominee Details') {
+      this.step1=false
+      this.step2=false
+      this.step3=true
+      this.step4=false
+      this.step5=false
+    } else if (stepper === 'Vehicle Details') {
+      this.step1=false
+      this.step2=false
+      this.step3=false
+      this.step4=true
+      this.step5=false
+    } else if (stepper === 'Previous Policy Details') {
+      this.step1=false
+      this.step2=false
+      this.step3=false
+      this.step4=false
+      this.step5=true
+    }
+  }
+
   /**
    * get ckyc data
    */
