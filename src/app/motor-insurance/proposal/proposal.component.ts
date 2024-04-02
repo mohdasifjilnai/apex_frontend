05aff40a2a67cf1cb7e0ddbece3734a4b5cd80a0
shivam <shivam.srivastava@renewbuy.com>
2024-04-02 17:49:10 +0530

    FIX :In case of OD nominee detail shown(which is not shown)

--- src/app/motor-insurance/proposal/proposal.component.ts
+++ src/app/motor-insurance/proposal/proposal.component.ts
@@ -72,7 +72,7 @@ export class ProposalComponent implements OnInit {
       for (let isCpa of this.quoteData?.premium_details
         ?.addon_premium_details) {
         if (
-          (isCpa?.add_on_code === 'CPA' || 'CPA3') &&
+          (isCpa?.add_on_code === 'CPA' || isCpa?.add_on_code === 'CPA3') &&
           this.proposerType !== 'corporate'
         ) {
           this.isNotShowNomineeDetails = true;
