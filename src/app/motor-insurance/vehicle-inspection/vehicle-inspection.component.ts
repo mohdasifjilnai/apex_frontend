512c44c4acc1ec29016b2e03d4e4e294d11251e9
Sushma Shukla <sushma.shukla@renewbuy.com>
2024-04-02 06:27:45 +0000

    FIX : inspection page issue for dynamic insurer name

--- src/app/motor-insurance/vehicle-inspection/vehicle-inspection.component.ts
+++ src/app/motor-insurance/vehicle-inspection/vehicle-inspection.component.ts
@@ -10,9 +10,13 @@ export class VehicleInspectionComponent implements OnInit {
   constructor(public router: Router) {}
   inspection: any;
   inspectionList: any;
+  quotesData: any;
+  insurerData: any;
   ngOnInit(): void {
     this.inspection = sessionStorage.getItem('breakIn');
     this.inspectionList = JSON.parse(this.inspection);
+    this.quotesData = sessionStorage.getItem('quotes_data');
+    this.insurerData = JSON.parse(this.quotesData);
   }
   back() {
     this.router.navigate(['/motor/quotes/proposal']);
