fe8168d0d08dc0aeaad98d51ad677330de617b7a
Sushma Shukla <sushma.shukla@renewbuy.com>
2024-04-24 12:16:42 +0000

    FIX : proposal type issue in quotes page

--- src/app/motor-insurance/quotes-listing/quotes-listing.component.ts
+++ src/app/motor-insurance/quotes-listing/quotes-listing.component.ts
@@ -304,10 +304,10 @@ export class QuotesListingComponent implements OnInit {
               sessionStorage.setItem('proposerType', this.owner_type);
             }
           } else {
-            this.quotesListing.patchValue({
-              proposalType: 1,
-            });
             this.owner_type = this.proposalList[0]?.proposer_name;
+            this.quotesListing.patchValue({
+              proposalType: this.proposalList[0].proposer_id,
+            });
             let proposalTypeValue = sessionStorage.getItem('proposerType');
             if (!proposalTypeValue) {
               sessionStorage.setItem('proposerType', this.owner_type);
