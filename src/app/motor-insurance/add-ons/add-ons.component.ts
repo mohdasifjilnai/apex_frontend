a707295b078973a7366a3155e4ea1bcb2a6cf2e2
Krishna Kumar <krishna.kumar@renewbuy.com>
2024-05-07 18:20:59 +0530

    FIX : Geological location bug fixes on quotes Page

--- src/app/motor-insurance/add-ons/add-ons.component.ts
+++ src/app/motor-insurance/add-ons/add-ons.component.ts
@@ -235,6 +235,7 @@ export class AddOnsComponent implements OnInit {
    * this fucntion use for clear all check box to uncheck
    */
   clearAllChecked() {
+    this.sharedDataService.sendCarLoaderMessage(0);
     for (let i = 0; i <= this.addOnsArray.length - 1; i++) {
       for (let j = 0; j <= this.addOnsArray[i].fe_template.length - 1; j++) {
         this.addOnsArray[i].fe_template[j].checked = false;
@@ -679,7 +680,10 @@ export class AddOnsComponent implements OnInit {
       /**
        * Find the index of the object that meets the condition
        */
-
+      const index = this.multipCheckboxName.indexOf(multiCheckbox_name);
+      if (index !== -1) {
+        this.multipCheckboxName.splice(index, 1);
+      }
       const indexMultiCheckoxRemove = this.subCheckBox.findIndex(
         (item: any) => {
           if (item === event?.source?.id) {
