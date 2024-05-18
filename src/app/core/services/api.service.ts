7dd1a0dfad7ad948fa9ab74310f3ceca5d6ec70c
Krishna Kumar <krishna.kumar@renewbuy.com>
2024-05-18 09:05:57 +0000

    FIX : SBI File size message change from 5MB to 2MB

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -83,7 +83,7 @@ export class ApiService {
             width: 'auto',
             height: 'auto',
             data: {
-              errorData: 'File size should not be more than 5MB',
+              errorData: 'File size should not be more than 2MB',
               statusdata: status,
             },
             panelClass: 'failure-dialog-class',
