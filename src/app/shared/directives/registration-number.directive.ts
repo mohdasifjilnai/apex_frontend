d677bec7c95cd320219e5f3bf4c3ce2f30888a72
Krishna Kumar <krishna.kumar@renewbuy.com>
2024-05-22 05:07:22 +0000

    FIX : Paste Registration number and Quotes Coming with wrong vehicle number

--- src/app/shared/directives/registration-number.directive.ts
+++ src/app/shared/directives/registration-number.directive.ts
@@ -35,12 +35,26 @@ export class RegistrationNumberDirective {
     if (clipboardData) {
       const pastedText = clipboardData.getData('text');
       let formattedText = pastedText
-        .replace(/-/g, '')
-        .replace(/(.{2})/g, '$1-');
         
-      formattedText = formattedText.slice(0, -4) + formattedText.slice(-3,-1);
+      // formattedText = formattedText.slice(0, -4) + formattedText.slice(-3,-1);
+      
+      const sanitizedValue = formattedText.replace(/-/g, '');
+
+      if (sanitizedValue.length < 7) {
+        const formattedValue = sanitizedValue.replace(/(.{2})/g, '$1-');
+        formattedText = formattedValue.replace(/-$/, '');
+      } else {
+        // Add hyphen between consecutive numbers or letters after the 4th character
+        const prefix = sanitizedValue.substring(0, 2);
+        const prefix2 = sanitizedValue.substring(2, 4);
+        const postfix = sanitizedValue.substring(4);
+        const formattedPostfix = postfix.replace(/([A-Za-z]+|[0-9]+)/g, '$1-');
+        formattedText = `${prefix}-${prefix2}-${formattedPostfix.replace(/-$/, '')}`;
+      }
       document.execCommand('insertText', false, formattedText);
       event.preventDefault();
     }
-  }
+    
+    }
+  
 }
