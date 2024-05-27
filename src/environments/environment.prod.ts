f1984d2d904f9dde83b909714058a6f9ed321589
Sushma Shukla <sushma.shukla@renewbuy.com>
2024-05-27 07:36:57 +0000

    ADD : environment file changes for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
   production: true,
+  baseUrl: 'https://apex.renewbuy.com/',
+  amsurl: 'https://accounts.rbstaging.in/',
+  artivatic_flag: false,
+  apex: 'https://apex.renewbuy.com/',
+  profile_redirection: 'https://partners.rbstaging.in',
 };
