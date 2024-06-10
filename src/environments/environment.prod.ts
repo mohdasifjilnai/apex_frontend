c56e4b5549321ab009f7decb506fb1672494f074
Sushma Shukla <sushma.shukla@renewbuy.com>
2024-06-10 14:52:31 +0000

    FIX : login popup redirection

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   amsurl: 'https://accounts.rbstaging.in/',
   artivatic_flag: false,
   apex: 'https://apex.renewbuy.com/',
-  profile_redirection: 'https://partners.rbstaging.in/home',
+  profile_redirection: 'https://partners.renewbuy.com/v2/',
   backend_url: 'https://apex.renewbuyinsurance.com',
   partner_v2: 'https://partners.renewbuy.com/v2/',
 };
