49717b601b6a7272f88431af18afb5395a1c62de
Sushma Shukla <sushma.shukla@renewbuy.com>
2024-06-13 09:46:15 +0000

    Revert "FIX : ncb related change in case of claimed is yes"

--- src/app/shared/components/form_fields/registration-year/registration-year.component.ts
+++ src/app/shared/components/form_fields/registration-year/registration-year.component.ts
@@ -244,15 +244,4 @@ export class RegistrationYearComponent implements OnInit {
     return date < this.disableFromDate || date >= dayBeforeCurrDate;
     //return date < this.disableFromDate || date > this.currDate;
   };
-
-  registrationDateSelected(data: any) {
-    // let registrationDate = this.form.controls['registration_date'].value;
-    // if (this.urlDate == 'motor') {
-    //   this.sharedDataService.registrationYearData(
-    //     this.form.controls['registration_date']
-    //   );
-    // } else {
-    //   this.onRegistrationDateChange(registrationDate);
-    // }
-  }
 }
