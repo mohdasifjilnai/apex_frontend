ab4ef22be2df38c429c96638a8122dfb4af06004
shivam <shivam.srivastava@renewbuy.com>
2024-06-19 12:20:14 +0530

    ADD :Script added only for production

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,53 +26,53 @@ export class AppComponent implements OnInit {
         return;
       }
     });
-    // if (environment.production) {
-    //   this.loadScript(
-    //     'https://www.googletagmanager.com/gtag/js?id=G-CTWKWSPJQ1',
-    //     true
-    //   )
-    //     .then(() => {
-    //       this.addGtagConfig();
-    //       this.checkScriptPresence(
-    //         'https://www.googletagmanager.com/gtag/js?id=G-CTWKWSPJQ1'
-    //       );
-    //     })
-    //     .catch((error) => {
-    //       console.error('Error loading the script:', error);
-    //     });
-    // }
+    if (environment.production) {
+      this.loadScript(
+        'https://www.googletagmanager.com/gtag/js?id=G-CTWKWSPJQ1',
+        true
+      )
+        .then(() => {
+          this.addGtagConfig();
+          this.checkScriptPresence(
+            'https://www.googletagmanager.com/gtag/js?id=G-CTWKWSPJQ1'
+          );
+        })
+        .catch((error) => {
+          console.error('Error loading the script:', error);
+        });
+    }
   }
 
-  // loadScript(url: string, isAsync: boolean = false): Promise<void> {
-  //   return new Promise((resolve, reject) => {
-  //     const script = document.createElement('script');
-  //     script.src = url;
-  //     if (isAsync) {
-  //       script.async = true;
-  //     }
-  //     script.onload = () => {
-  //       resolve();
-  //     };
-  //     script.onerror = () => {
-  //       reject(new Error(`Failed to load script: ${url}`));
-  //     };
-  //     document.head.appendChild(script);
-  //   });
-  // }
+  loadScript(url: string, isAsync: boolean = false): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = url;
+      if (isAsync) {
+        script.async = true;
+      }
+      script.onload = () => {
+        resolve();
+      };
+      script.onerror = () => {
+        reject(new Error(`Failed to load script: ${url}`));
+      };
+      document.head.appendChild(script);
+    });
+  }
 
-  // addGtagConfig(): void {
-  //   const script = document.createElement('script');
-  //   script.innerHTML = `
-  //     window.dataLayer = window.dataLayer || [];
-  //     function gtag(){dataLayer.push(arguments);}
-  //     gtag('js', new Date());
-  //     gtag('config', 'G-CTWKWSPJQ1');
-  //   `;
-  //   document.head.appendChild(script);
-  // }
+  addGtagConfig(): void {
+    const script = document.createElement('script');
+    script.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-CTWKWSPJQ1');
+    `;
+    document.head.appendChild(script);
+  }
 
-  // private checkScriptPresence(src: string): void {
-  //   const scripts = Array.from(document.getElementsByTagName('script'));
-  //   const scriptExists = scripts.some((script) => script.src.includes(src));
-  // }
+  private checkScriptPresence(src: string): void {
+    const scripts = Array.from(document.getElementsByTagName('script'));
+    const scriptExists = scripts.some((script) => script.src.includes(src));
+  }
 }
