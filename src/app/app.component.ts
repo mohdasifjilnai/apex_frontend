4b0fa4e4823b70f0d7b555a626aa8d2949c1800f
shivam <shivam.srivastava@renewbuy.com>
2024-06-19 10:43:58 +0530

    ADD :Script added only for production

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoaderService } from './core/services/loader.service';
 import { SseService } from './core/services/sse.service';
+import { environment } from 'src/environments/environment';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'apex_frontend';
   connectionData: any;
   eventSource: any;
-
-  constructor(private sseService: SseService,private loaderService: LoaderService) {}
   isLoading: boolean = false;
+
+  constructor(
+    private sseService: SseService,
+    private loaderService: LoaderService
+  ) {}
+
   ngOnInit(): void {
-    this.loaderService.isLoading().subscribe((isLoading:any) => {
+    this.loaderService.isLoading().subscribe((isLoading: any) => {
       this.isLoading = isLoading;
-      if(!isLoading){
-        return
+      if (!isLoading) {
+        return;
       }
     });
+    if (environment.production) {
+      this.loadScript(
+        'https://www.googletagmanager.com/gtag/js?id=G-CTWKWSPJQ1',
+        true
+      )
+        .then(() => {
+          this.addGtagConfig();
+          this.checkScriptPresence(
+            'https://www.googletagmanager.com/gtag/js?id=G-CTWKWSPJQ1'
+          );
+        })
+        .catch((error) => {
+          console.error('Error loading the script:', error);
+        });
+    }
+  }
+
+  loadScript(url: string, isAsync: boolean = false): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = url;
+      if (isAsync) {
+        script.async = true;
+      }
+      script.onload = () => {
+        resolve();
+      };
+      script.onerror = () => {
+        reject(new Error(`Failed to load script: ${url}`));
+      };
+      document.head.appendChild(script);
+    });
+  }
+
+  addGtagConfig(): void {
+    const script = document.createElement('script');
+    script.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-CTWKWSPJQ1');
+    `;
+    document.head.appendChild(script);
+  }
+
+  private checkScriptPresence(src: string): void {
+    const scripts = Array.from(document.getElementsByTagName('script'));
+    const scriptExists = scripts.some((script) => script.src.includes(src));
   }
 }
