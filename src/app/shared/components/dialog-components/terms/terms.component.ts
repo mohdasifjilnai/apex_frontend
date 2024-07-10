2526e3534129a646c6f9a55bedb52cda732c9050
Krishna Kumar <krishna.kumar@renewbuy.com>
2024-07-10 05:01:21 +0000

    ADD : T&C and UI  in footer and quotes for Payout

--- src/app/shared/components/dialog-components/terms/terms.component.ts
+++ src/app/shared/components/dialog-components/terms/terms.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
+import { MAT_BOTTOM_SHEET_DATA, MatBottomSheetRef } from '@angular/material/bottom-sheet';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-terms',
@@ -9,11 +10,16 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 })
 export class TermsComponent implements OnInit {
   insurerCode: any;
+  currentUrl: any;
+  extractedPath: any;
   constructor(
     public dialogRef: MatDialogRef<TermsComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
+    private router: Router,
+    @Inject(MAT_BOTTOM_SHEET_DATA) public bottomSheetdata: any,
     public bottomSheetRef: MatBottomSheetRef<TermsComponent>
-  ) {}
+  ) {
+  }
 
   ngOnInit(): void {
     const kycData = JSON.parse(sessionStorage.getItem('kycData') || '{}');
--- src/app/ui/footer/footer.component.ts
+++ src/app/ui/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatBottomSheet, MatBottomSheetConfig } from '@angular/material/bottom-sheet';
+import { NavigationEnd, Router } from '@angular/router';
+import { TermsComponent } from 'src/app/shared/components/dialog-components/terms/terms.component';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
+  currentUrl: any;
+  extractedPath: any;
+  constructor(private router: Router,public bottomSheet: MatBottomSheet,) { 
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentUrl = this.router.url;
+        const urlParts = this.currentUrl.split('/');
+        this.extractedPath=urlParts.length
+      }
+    });
+  }
 
   ngOnInit(): void {
+
+  }
+  termsCondition(){
+    if (window.innerWidth <= 999) {
+      const bottomSheetConfig: MatBottomSheetConfig = {
+        data: 'footer',
+      };
+      this.bottomSheet.open(TermsComponent,bottomSheetConfig);
+    }
   }
 
 }
