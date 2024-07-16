6789d04f1e0960c923df7a5dc9c60d1a981c8446
Krishna Kumar <krishna.kumar@renewbuy.com>
2024-07-16 11:52:48 +0530

    FIX : Manual Ckyc for bajaj in Corporate Case

--- src/app/shared/components/dialog-components/ckyc-documents/ckyc-documents.component.ts
+++ src/app/shared/components/dialog-components/ckyc-documents/ckyc-documents.component.ts
@@ -332,7 +332,7 @@ handles the form submit for uploading the required documents
           : null,
         poi_document: {
           poi_type: this.showPOI
-            ? this.uploadDocumentsForm.get('poi_type')?.value
+            ? this.uploadDocumentsForm.get('document_type_based_field')?.value
             : null,
           poi_no: this.uploadDocumentsForm.get('poi_no')?.value
             ? this.uploadDocumentsForm.get('poi_no')?.value
