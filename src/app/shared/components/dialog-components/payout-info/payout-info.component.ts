8ed98d324186f5bd6e1a1b3b315f0b58a539f437
Krishna Kumar <krishna.kumar@renewbuy.com>
2024-07-31 17:16:47 +0530

    FIX : Payout Info UI Issue fixed

--- src/app/shared/components/dialog-components/payout-info/payout-info.component.ts
+++ src/app/shared/components/dialog-components/payout-info/payout-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-payout-info',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PayoutInfoComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<PayoutInfoComponent>,) { }
 
   ngOnInit(): void {
   }
-
+ /**
+   * this fucntion use for close pop up
+   */
+ onClose(): void {
+  this.dialogRef.close();
+}
 }
