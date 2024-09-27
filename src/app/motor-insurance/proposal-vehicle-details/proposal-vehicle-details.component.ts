955a1cbf2fd88c5e30cf466049dae8ab2f5ccc25
sushma <sushma.shukla@renewbuy.com>
2024-09-27 16:45:51 +0530

    FIX : payment issue and rollover issue for registration number

--- src/app/motor-insurance/proposal-vehicle-details/proposal-vehicle-details.component.ts
+++ src/app/motor-insurance/proposal-vehicle-details/proposal-vehicle-details.component.ts
@@ -15,6 +15,7 @@ import {
   ValidatorFn,
   Validators,
 } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import moment from 'moment';
 import {
@@ -28,6 +29,9 @@ import {
 import { ApiConstants } from 'src/app/api.constant';
 import { ApiService } from 'src/app/core/services/api.service';
 import { SharedDataService } from 'src/app/core/services/shared-data.service';
+import { WindowRef } from 'src/app/core/services/window-ref.service';
+import { ErrorDialogComponent } from 'src/app/shared/components/dialog-components/error-dialog/error-dialog.component';
+import { FailureDialogComponent } from 'src/app/shared/components/dialog-components/failure-dialog/failure-dialog.component';
 
 @Component({
   selector: 'app-proposal-vehicle-details',
@@ -75,13 +79,31 @@ export class ProposalVehicleDetailsComponent implements OnInit {
   maxlength: any;
   isOwnerAddressValidation: boolean = false;
   regNumber: any;
-  previousDetails:any;
+  previousDetails: any;
   details: any;
+  partnerCodewithTraceId: any;
+  failureJSON: {
+    modalName: any;
+    widthObtained: string;
+    heightObtained: string;
+    topObtained: string;
+    isOutSideClose: boolean;
+    classObtained: string;
+  } = {
+    modalName: ErrorDialogComponent,
+    widthObtained: 'auto',
+    heightObtained: 'auto',
+    topObtained: 'auto',
+    isOutSideClose: true,
+    classObtained: 'nonPOS-class',
+  };
   constructor(
     private apiservice: ApiService,
     private shareData: SharedDataService,
     private router: Router,
-    private formBuild: FormBuilder
+    private formBuild: FormBuilder,
+    public dialogRef: MatDialogRef<ProposalVehicleDetailsComponent>,
+    private matDialog: WindowRef
   ) {
     this.proposalForm();
   }
@@ -120,15 +142,18 @@ export class ProposalVehicleDetailsComponent implements OnInit {
       registration_number_last_digit: [''],
     });
   }
-  regNumbers:any;
+  regNumbers: any;
   ngOnInit(): void {
     this.quoteData = sessionStorage.getItem('quotes_data');
     if (this.quoteData) {
       this.insurerCode = JSON.parse(this.quoteData)['insurer_code'];
     }
-    if(this.insurerCode=='oriental'){
-      this.proposalVehilceDetailsForm.get('vehicle_colour')?.setValidators(Validators.required);
-      this.proposalVehilceDetailsForm.get('vehicle_colour')?.updateValueAndValidity;
+    if (this.insurerCode == 'oriental') {
+      this.proposalVehilceDetailsForm
+        .get('vehicle_colour')
+        ?.setValidators(Validators.required);
+      this.proposalVehilceDetailsForm.get('vehicle_colour')
+        ?.updateValueAndValidity;
     }
     this.previousInsurerCode = sessionStorage.getItem('previous_insurerCode');
     if (this.insurerCode === 'sbi_general') {
@@ -324,19 +349,19 @@ export class ProposalVehicleDetailsComponent implements OnInit {
         }
       } else {
         this.regNumber = sessionStorage.getItem('registrationNumber');
-        if(this.regNumber && renewalType != 'renewal'){
-        this.apiservice
-          .getRequestedResponse(
-            `${ApiConstants.registration_number}?regn_no=${this.regNumber}`
-          )
-          .subscribe((res: any) => {
-            if (res) {
-              this.proposalVehilceDetailsForm.patchValue({
-                chassis_number: res?.chassis_number,
-                engine_number: res?.engine_number,
-              });
-            }
-          });
+        if (this.regNumber && renewalType != 'renewal') {
+          this.apiservice
+            .getRequestedResponse(
+              `${ApiConstants.registration_number}?regn_no=${this.regNumber}`
+            )
+            .subscribe((res: any) => {
+              if (res) {
+                this.proposalVehilceDetailsForm.patchValue({
+                  chassis_number: res?.chassis_number,
+                  engine_number: res?.engine_number,
+                });
+              }
+            });
         }
       }
       if (
@@ -472,74 +497,71 @@ export class ProposalVehicleDetailsComponent implements OnInit {
     }
 
     this.previousDetails = sessionStorage.getItem('RenewalPreviousDetails');
-    if(this.previousDetails != null){
-      this.details = JSON.parse(this.previousDetails)
-      const vehicleDetails = this.details?.previous_policy_details?.vehicle_details;
-      let regNumbers = this.details?.previous_policy_details?.vehicle_details?.registration_no;
+    if (this.previousDetails != null) {
+      this.details = JSON.parse(this.previousDetails);
+      const vehicleDetails =
+        this.details?.previous_policy_details?.vehicle_details;
+      let regNumbers =
+        this.details?.previous_policy_details?.vehicle_details?.registration_no;
       if (regNumbers) {
         this.isRegistrationNumber = true;
         let regFirstDigit = regNumbers?.slice(0, 2);
         let regSecondDigit = regNumbers?.slice(2, 5);
         let combineRegData = regFirstDigit + '-' + regSecondDigit + '-';
-        let regLastDigit = regNumbers?.slice(5, );
-        
-      this.proposalVehilceDetailsForm.patchValue({
-            registration_number_first: regFirstDigit,
-            registration_number_second: regSecondDigit,
-            registration_number_last_digit: regLastDigit,
-            registration_date: vehicleDetails?.registration_date,
-            manufacture_date: vehicleDetails?.manufacture_date,
-            engine_number: vehicleDetails?.engine_no,
-            chassis_number: vehicleDetails?.chassis_no,
-            vehicle_pincode: vehicleDetails?.registration_address?.pincode,
-            vehilce_city: vehicleDetails?.registration_address?.rb_city_name,
-            vehicle_state: vehicleDetails?.registration_address?.rb_state_name,
-            financer: vehicleDetails?.financer_details?.financer_id,
-            agreement_type: vehicleDetails?.financer_details?.agreement_type,
-            financer_city: vehicleDetails?.financer_details?.financer_branch,
-            is_financed: vehicleDetails?.is_vehicle_financed,
-            vehicle_registration_address: vehicleDetails?.registration_address?.address_line,
-            is_vehicle_address: vehicleDetails?.is_same_location,
-            
-          });
-          if (
-            vehicleDetails?.registration_address?.pincode
-          ) {
-            this.apiservice
-              .getRequestedResponse(
-                `${ApiConstants.pincode}?pincode=${
-                  vehicleDetails?.registration_address?.pincode
-                }&insurer_code=${JSON.parse(this.quoteData)['insurer_code']}`
-              )
-              .subscribe((res) => {
-                this.proposalVehilceDetailsForm.patchValue({
-                  vehicle_pincode: res[0],
-                  vehilce_city: res[0].rb_city_name,
-                  vehicle_state: res[0].rb_state_name,
-                });
-                this.shareData?.sendOwnnerAddres(
-                  this.proposalVehilceDetailsForm.valid
-                );
+        let regLastDigit = regNumbers?.slice(5);
+
+        this.proposalVehilceDetailsForm.patchValue({
+          registration_number_first: regFirstDigit,
+          registration_number_second: regSecondDigit,
+          registration_number_last_digit: regLastDigit,
+          registration_date: vehicleDetails?.registration_date,
+          manufacture_date: vehicleDetails?.manufacture_date,
+          engine_number: vehicleDetails?.engine_no,
+          chassis_number: vehicleDetails?.chassis_no,
+          vehicle_pincode: vehicleDetails?.registration_address?.pincode,
+          vehilce_city: vehicleDetails?.registration_address?.rb_city_name,
+          vehicle_state: vehicleDetails?.registration_address?.rb_state_name,
+          financer: vehicleDetails?.financer_details?.financer_id,
+          agreement_type: vehicleDetails?.financer_details?.agreement_type,
+          financer_city: vehicleDetails?.financer_details?.financer_branch,
+          is_financed: vehicleDetails?.is_vehicle_financed,
+          vehicle_registration_address:
+            vehicleDetails?.registration_address?.address_line,
+          is_vehicle_address: vehicleDetails?.is_same_location,
+        });
+        if (vehicleDetails?.registration_address?.pincode) {
+          this.apiservice
+            .getRequestedResponse(
+              `${ApiConstants.pincode}?pincode=${
+                vehicleDetails?.registration_address?.pincode
+              }&insurer_code=${JSON.parse(this.quoteData)['insurer_code']}`
+            )
+            .subscribe((res) => {
+              this.proposalVehilceDetailsForm.patchValue({
+                vehicle_pincode: res[0],
+                vehilce_city: res[0].rb_city_name,
+                vehicle_state: res[0].rb_state_name,
               });
-            }
-            if (vehicleDetails?.financer_details?.financer_id) {
-              this.apiservice
-                .getRequestedResponse(
-                  `${ApiConstants.financier_List}?insurer_code=${
-                    JSON.parse(this.quoteData)['insurer_code']
-                  }&financier_id=${
-                    vehicleDetails?.financer_details?.financer_id
-                  }`
-                )
-                .subscribe((response) => {
-                  this.proposalVehilceDetailsForm.patchValue({
-                    financer: response[0],
-                  });
-                });
-            }
-          }
+              this.shareData?.sendOwnnerAddres(
+                this.proposalVehilceDetailsForm.valid
+              );
+            });
+        }
+        if (vehicleDetails?.financer_details?.financer_id) {
+          this.apiservice
+            .getRequestedResponse(
+              `${ApiConstants.financier_List}?insurer_code=${
+                JSON.parse(this.quoteData)['insurer_code']
+              }&financier_id=${vehicleDetails?.financer_details?.financer_id}`
+            )
+            .subscribe((response) => {
+              this.proposalVehilceDetailsForm.patchValue({
+                financer: response[0],
+              });
+            });
+        }
+      }
     }
-   
   }
 
   filterInsurer(name: string) {}
@@ -562,75 +584,98 @@ export class ProposalVehicleDetailsComponent implements OnInit {
     }
   }
   getProposalVehicleData(isValid: any) {
-    const vehcileType=sessionStorage.getItem('vehicleType');
-    const partnerCode=localStorage.getItem('partner_code')
+    const vehcileType = sessionStorage.getItem('vehicleType');
+    let partnerCode = localStorage.getItem('partner_code');
+    if (!partnerCode) {
+      this.partnerCodewithTraceId = JSON.parse(
+        sessionStorage.getItem('partnerCodeTraceId') || '{}'
+      );
+      if (this.partnerCodewithTraceId?.partner_code) {
+        partnerCode = this.partnerCodewithTraceId?.partner_code;
+      }
+    }
     let registrationNumberFirst =
-          this.divideString(this.mmvItem?.registration_city?.rb_rto_code)[0] +
-          '-' +
-          this.divideString(this.mmvItem?.registration_city?.rb_rto_code)[1] +
-          '-' +
-          this.proposalVehilceDetailsForm.value.registration_number_last_digit;
+      this.divideString(this.mmvItem?.registration_city?.rb_rto_code)[0] +
+      '-' +
+      this.divideString(this.mmvItem?.registration_city?.rb_rto_code)[1] +
+      '-' +
+      this.proposalVehilceDetailsForm.value.registration_number_last_digit;
     if (isValid) {
       const formValues = this.proposalVehilceDetailsForm.value;
       this.apiservice
-          .getRequestedResponse(
-            `${ApiConstants.renewal_partner_validation}?vehicle_type=${vehcileType}&registration_num=${this.proposalVehilceDetailsForm.value.registration_number_last_digit?registrationNumberFirst:''}&partner_code=${partnerCode?partnerCode:''}`
-          )
-          .subscribe((res) => {
-            if(res?.status){
-              if (
-                this.mmvItem &&
-                this.proposalVehilceDetailsForm.value.registration_number_last_digit
-              ) {
-                let registrationNumberFirst =
-                  this.divideString(this.mmvItem?.registration_city?.rb_rto_code)[0] +
-                  '-' +
-                  this.divideString(this.mmvItem?.registration_city?.rb_rto_code)[1] +
-                  '-' +
-                  this.proposalVehilceDetailsForm.value.registration_number_last_digit;
-                this.proposalVehilceDetailsForm.patchValue({
-                  registration_number: registrationNumberFirst,
-                });
-              }
-        
-              // this.shareData?.getAddressValidation(
-              //   JSON.parse(this.quoteData)['insurer_code']
-              // );
-        
-              this.afterVehicleData.emit(formValues);
-              this.shareData.createProposalId(
-                'vehilce_details',
-                this.proposalVehilceDetailsForm
-              );
-        
-              /**
-               * Unsubscribe before subscribing to avoid multiple subscriptions
-               */
-              if (this.proposalDetailsSubscription) {
-                this.proposalDetailsSubscription.unsubscribe();
-              }
-        
-              /**
-               * subscribe to getProposalDetails and navigate after the response
-               */
-              this.proposalDetailsSubscription =
-                this.shareData.getProposalDetails.subscribe((proposal) => {
-                  if (
-                    (this.vehicleType === 'new' ||
-                      (this.isBreakIn && this.productTypeValue === 'satp') ||
-                      this.mmvItem?.policy_expiry === 'IDK') &&
-                    proposal.vehicle_details !== null
-                  ) {
-                    const proposal_id=sessionStorage.getItem('proposal_Id')
-                    this.shareData.crossSellRecomendation(proposal_id)
-                    this.router.navigate([
-                      `quotes/proposal/${this.transactionId}/review`,
-                    ]);
-                    this.proposalDetailsSubscription.unsubscribe();
-                  }
-                });
+        .getRequestedResponse(
+          `${
+            ApiConstants.renewal_partner_validation
+          }?vehicle_type=${vehcileType}&registration_num=${
+            this.proposalVehilceDetailsForm.value.registration_number_last_digit
+              ? registrationNumberFirst
+              : ''
+          }&partner_code=${partnerCode ? partnerCode : ''}`
+        )
+        .subscribe((res) => {
+          if (res?.status) {
+            if (
+              this.mmvItem &&
+              this.proposalVehilceDetailsForm.value
+                .registration_number_last_digit
+            ) {
+              let registrationNumberFirst =
+                this.divideString(
+                  this.mmvItem?.registration_city?.rb_rto_code
+                )[0] +
+                '-' +
+                this.divideString(
+                  this.mmvItem?.registration_city?.rb_rto_code
+                )[1] +
+                '-' +
+                this.proposalVehilceDetailsForm.value
+                  .registration_number_last_digit;
+              this.proposalVehilceDetailsForm.patchValue({
+                registration_number: registrationNumberFirst,
+              });
             }
-          });
+
+            // this.shareData?.getAddressValidation(
+            //   JSON.parse(this.quoteData)['insurer_code']
+            // );
+
+            this.afterVehicleData.emit(formValues);
+            this.shareData.createProposalId(
+              'vehilce_details',
+              this.proposalVehilceDetailsForm
+            );
+
+            /**
+             * Unsubscribe before subscribing to avoid multiple subscriptions
+             */
+            if (this.proposalDetailsSubscription) {
+              this.proposalDetailsSubscription.unsubscribe();
+            }
+
+            /**
+             * subscribe to getProposalDetails and navigate after the response
+             */
+            this.proposalDetailsSubscription =
+              this.shareData.getProposalDetails.subscribe((proposal) => {
+                if (
+                  (this.vehicleType === 'new' ||
+                    (this.isBreakIn && this.productTypeValue === 'satp') ||
+                    this.mmvItem?.policy_expiry === 'IDK') &&
+                  proposal.vehicle_details !== null
+                ) {
+                  const proposal_id = sessionStorage.getItem('proposal_Id');
+                  this.shareData.crossSellRecomendation(proposal_id);
+                  this.router.navigate([
+                    `quotes/proposal/${this.transactionId}/review`,
+                  ]);
+                  this.proposalDetailsSubscription.unsubscribe();
+                }
+              });
+          } else {
+            this.failureJSON['modalName'] = FailureDialogComponent;
+            this.openFailurePopup(res);
+          }
+        });
     }
   }
   /**
@@ -1001,5 +1046,31 @@ export class ProposalVehicleDetailsComponent implements OnInit {
       // Update the control validity status
       ownerCommunicationAddressControl.updateValueAndValidity();
     }
+  }
+
+  openFailurePopup(objData: any) {
+    let resWidth;
+    let resTop;
+    if (window.screen.width <= 767) {
+      resWidth = 'auto';
+      resTop = '5%';
+    } else {
+      resWidth = 'auto';
+      resTop = '5%';
+    }
+    const obj: any = {
+      modalName: this.failureJSON['modalName'],
+      width: this.failureJSON['widthObtained'],
+      height: this.failureJSON['heightObtained'],
+      classNameObtained: this.failureJSON['classObtained'],
+      isOutSideClose: this.failureJSON['isOutSideClose'],
+      minWidth: resWidth,
+      dataInfo: {
+        data: objData,
+        top: resTop,
+      },
+    };
+
+    this.matDialog.openDialog(obj);
   }
 }
--- src/app/shared/components/dialog-components/failure-dialog/failure-dialog.component.ts
+++ src/app/shared/components/dialog-components/failure-dialog/failure-dialog.component.ts
@@ -21,9 +21,10 @@ export class FailureDialogComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public data: any,
     private matDialog: WindowRef,
     private sharedService: SharedDataService,
-    private route:Router
+    private route: Router
   ) {
     this.transactionId = sessionStorage.getItem('transaction_id');
+
     this.faliureData = data['data'];
     if (this.faliureData?.error_message) {
       this.errorMessage = this.faliureData?.error_message;
@@ -34,12 +35,19 @@ export class FailureDialogComponent implements OnInit {
       this.errorMessage = data['errorData']['message'];
     } else if (data?.statusdata?.status == 409) {
       this.errorMessage = data['errorData']['message'];
-      if(data['errorData']['code']==1008){
+      if (data['errorData']['code'] == 1008) {
+        if (!this.transactionId) {
+          const url = this.route.url;
+          const parts = url.split('/');
+          this.transactionId = parts[3];
+        }
         this.dialogRef.close();
-        this.route.navigate([`quotes/proposal/${this.transactionId}/review/payment-success`])
+
+        this.route.navigate([
+          `quotes/proposal/${this.transactionId}/review/payment-success`,
+        ]);
       }
-    }
-    else if (
+    } else if (
       (data?.data?.status == false && data?.data?.insurer_code != 'digit') ||
       data?.data?.err_code == 1
     ) {
