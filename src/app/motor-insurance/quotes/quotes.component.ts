dbc5fb2d681165b84dfab84bb6357fc9f8cdffe2
Krishna Kumar <krishna.kumar@renewbuy.com>
2024-10-17 12:49:03 +0530

    FIX : Registration NUmber Not Passing in initiate Quotes on Url Share

--- src/app/motor-insurance/quotes/quotes.component.ts
+++ src/app/motor-insurance/quotes/quotes.component.ts
@@ -398,6 +398,7 @@ export class QuotesComponent implements OnInit {
           sessionStorage.setItem('productType', res.product_type);
           sessionStorage.setItem('transaction_id', res.transaction_id);
           sessionStorage.setItem('newVehicleType', res.business_type);
+          sessionStorage.setItem('registrationNumber', res.registration_no);
           if (res?.partner_code == null) {
             localStorage.setItem('partner_code', '');
           } else {
