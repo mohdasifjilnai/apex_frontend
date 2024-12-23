9462997a5642512c34fcfa3111a705db94d41f95
Krishna Kumar <krishna.kumar@renewbuy.com>
2024-12-23 11:36:15 +0000

    FIX : CV Journey Disabled on dev Journey

--- src/app/shared/components/form_fields/vehicle-type/vehicle-type.component.ts
+++ src/app/shared/components/form_fields/vehicle-type/vehicle-type.component.ts
@@ -32,26 +32,13 @@ export class VehicleTypeComponent implements OnInit {
     })
   }
 
-  selectVehicle(vehicle: any,index: number) {
-    if(this.env.apex!='https://dev-apex.rbstaging.in/' || this.env.apex_local!='http://test.rbstaging.in/' ){
-      if(index!=2){
-        this.selectedVehicleType = vehicle;
-
+  selectVehicle(vehicle: any) {
+    this.selectedVehicleType = vehicle;
     sessionStorage.setItem(
       'vehicleType',
       `${this.selectedVehicleType.optionNameValue}`
     );
     this.sharedata.selectedvehicle(this.selectedVehicleType.optionNameValue);
-      }
-    }else{
-      this.selectedVehicleType = vehicle;
-
-      sessionStorage.setItem(
-        'vehicleType',
-        `${this.selectedVehicleType.optionNameValue}`
-      );
-      this.sharedata.selectedvehicle(this.selectedVehicleType.optionNameValue);
-    }    
   }
   isLast(index: number): boolean {
     return index === this.vehicleTypeListData.vehicleTypeList.length - 1;
