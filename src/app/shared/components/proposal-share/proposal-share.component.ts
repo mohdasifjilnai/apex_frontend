bca99728b189ea6ad9828d4a9e8812178fd4c5a5
Krishna Kumar <krishna.kumar@renewbuy.com>
2025-02-19 11:07:54 +0000

    FIX : Proposal Share Popup closing ISsue

--- src/app/shared/components/proposal-share/proposal-share.component.ts
+++ src/app/shared/components/proposal-share/proposal-share.component.ts
@@ -301,6 +301,7 @@ export class ProposalShareComponent implements OnInit {
         )
         .subscribe((res) => {
           this.loader = false;
+          this.onClose();
           if (window.innerWidth <= 999) {
             const bottomSheetConfig: MatBottomSheetConfig = {
               data: sendCommunicationObject, // Pass your data here
@@ -338,6 +339,7 @@ export class ProposalShareComponent implements OnInit {
         )
         .subscribe((res) => {
           this.loader = false;
+          this.onClose();
           if (window.innerWidth <= 999) {
             const bottomSheetConfig: MatBottomSheetConfig = {
               data: sendCommunicationObject, // Pass your data here
