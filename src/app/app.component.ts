cc56d5c3a79a512fc2157acb271f86a21e8d8da4
sushma <sushma.shukla@renewbuy.com>
2025-04-11 11:10:34 +0530

    ADD : slient login and increare chassis no limit in proposal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,13 @@ export class AppComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
     if (environment?.dev) {
       // webengage.init('in~~10a5cbbcb');
-      console.log(webengage.util.getWebengageCookie().cuid);
+
+      if (
+        webengage.util.getWebengageCookie().cuid == undefined &&
+        sessionStorage.getItem('partner_code')
+      ) {
+        webengage.user.login(sessionStorage.getItem('partner_code'));
+      }
     } else {
       // webengage.init('');
     }
--- src/app/motor-insurance/proposal-vehicle-details/proposal-vehicle-details.component.ts
+++ src/app/motor-insurance/proposal-vehicle-details/proposal-vehicle-details.component.ts
@@ -124,7 +124,7 @@ export class ProposalVehicleDetailsComponent implements OnInit {
         [
           Validators.required,
           Validators.minLength(6),
-          Validators.maxLength(25),
+          Validators.maxLength(30),
         ],
       ],
       registration_date: ['', Validators.required],
