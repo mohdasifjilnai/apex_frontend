17701fb3a0feba8e28f0e795ec44058c6b58cc31
sushma <sushma.shukla@renewbuy.com>
2025-11-19 11:05:21 +0530

    FIX : payment page issue related to trace id

--- src/app/ui/breadcrumb/breadcrumb.component.ts
+++ src/app/ui/breadcrumb/breadcrumb.component.ts
@@ -113,6 +113,26 @@ export class BreadcrumbComponent implements OnInit {
         if (res?.employee_code != null) {
           sessionStorage.setItem('employee_code', res?.employee_code);
         }
+
+        if (res?.trace_id) {
+          this.getTraceIdData(res.trace_id);
+        }
+      });
+  }
+
+  getTraceIdData(trace_id: any) {
+    let apiUrl;
+
+    apiUrl = `?trace_id=${trace_id}`;
+    this.apiService
+      .getRequestedResponse(`${ApiConstants.get_trace_Id()}${apiUrl}`)
+      .subscribe((res: any) => {
+        if (res?.message != 'Partner not found') {
+          sessionStorage.setItem('encrypttoken', res?.encrypted_token);
+          if (res?.source) {
+            sessionStorage.setItem('sourceValue', res?.source);
+          }
+        }
       });
   }
 }
